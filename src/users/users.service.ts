43dab3b2000997c0a13fae0e34359699f3d0fc4e
josesantana.web <josesantana.web@gmail.com>
2022-03-12 00:07:08 -0400

    feat: add user module

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({ data: createUserDto });
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prismaService.user.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prismaService.user.update({
+      data: updateUserDto,
+      where: { id },
+    });
+  }
+
+  remove(id: number) {
+    return this.prismaService.user.delete({ where: { id } });
+  }
+}
