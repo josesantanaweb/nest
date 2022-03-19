6bcd52893e26f20c50fc38b6a7afd01eea8e1a43
josesantana.web <josesantana.web@gmail.com>
2022-03-19 18:43:00 -0400

    faet: add roles decorators

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -6,35 +6,50 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { RolesAllowed } from '../auth/decorators/roles.decorator';
+import { Roles } from '../auth/constants';
+import { RoleBasedGuard } from '../auth/guards/roles.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
+  @RolesAllowed(Roles.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleBasedGuard)
   @Post()
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.rolesService.create(createRoleDto);
   }
 
+  @RolesAllowed(Roles.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleBasedGuard)
   @Get()
   findAll() {
     return this.rolesService.findAll();
   }
 
+  @RolesAllowed(Roles.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleBasedGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.rolesService.findOne(+id);
   }
 
+  @RolesAllowed(Roles.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleBasedGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
     return this.rolesService.update(+id, updateRoleDto);
   }
 
+  @RolesAllowed(Roles.ADMIN)
+  @UseGuards(JwtAuthGuard, RoleBasedGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.rolesService.remove(+id);
